1d14a01b44c62353b088df498ffe5ff911ba2612
wochatme <wochatdb@gmail.com>
2024-08-04 15:19:26 -0700

    Init commit

---
+++ win/MainFrm.h
@@ -0,0 +1,62 @@
+// MainFrm.h : interface of the CMainFrame class
+//
+/////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+class CMainFrame : 
+	public CFrameWindowImpl<CMainFrame>, 
+	public CUpdateUI<CMainFrame>,
+	public CMessageFilter, public CIdleHandler
+{
+public:
+	DECLARE_FRAME_WND_CLASS(NULL, IDR_MAINFRAME)
+
+	CView m_view;
+
+	virtual BOOL PreTranslateMessage(MSG* pMsg)
+	{
+		if(CFrameWindowImpl<CMainFrame>::PreTranslateMessage(pMsg))
+			return TRUE;
+
+		return FALSE;
+	}
+
+	virtual BOOL OnIdle()
+	{
+		return FALSE;
+	}
+
+	BEGIN_UPDATE_UI_MAP(CMainFrame)
+	END_UPDATE_UI_MAP()
+
+	BEGIN_MSG_MAP(CMainFrame)
+		MESSAGE_HANDLER(WM_CREATE, OnCreate)
+		MESSAGE_HANDLER(WM_DESTROY, OnDestroy)
+		CHAIN_MSG_MAP(CUpdateUI<CMainFrame>)
+		CHAIN_MSG_MAP(CFrameWindowImpl<CMainFrame>)
+	END_MSG_MAP()
+
+	LRESULT OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& /*bHandled*/)
+	{
+		// register object for message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->AddMessageFilter(this);
+		pLoop->AddIdleHandler(this);
+
+		return 0;
+	}
+
+	LRESULT OnDestroy(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/, BOOL& bHandled)
+	{
+		// unregister message filtering and idle updates
+		CMessageLoop* pLoop = _Module.GetMessageLoop();
+		ATLASSERT(pLoop != NULL);
+		pLoop->RemoveMessageFilter(this);
+		pLoop->RemoveIdleHandler(this);
+
+		bHandled = FALSE;
+		return 1;
+	}
+};
